b0edd1575869105e56d7a9dcdfccb50c10e713df
Thomaspeel6 <146444012+Thomaspeel6@users.noreply.github.com>
2023-10-10 15:29:23 +0000

    as defasd

---
+++ week2_c_bootcamp/third.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main () 
+{
+    int var1, var2;
+    printf("insert 2 numbers: ");
+    scanf("%d %d", &var1, &var2);
+    printf("answer = %d + %d\n", var1, var2);
+    return 0;
+}
